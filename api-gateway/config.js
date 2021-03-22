8dbcfeeb6c01e12ce24d2f50318141c487148ae7
Adam Luna <adluna@my365.bellevue.edu>
2021-03-22 00:15:22 -0500

    Add 1.4

---
+++ api-gateway/config.js
@@ -0,0 +1,16 @@
+/*
+============================================
+; Title: config.js
+; Author: Adam Luna
+; Date: 21 March 2021
+; Description: config.js file for assignment 1.4
+;===========================================
+*/
+
+var config = {};
+
+config.web = {};
+
+config.web.port = process.env.PORT || "3000";
+
+module.exports = config;
